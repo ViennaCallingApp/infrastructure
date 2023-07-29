f9e895292214b0077718f6b5f92b28d9eff8736a
Jan Jaworski <bleggbeard@protonmail.com>
2023-07-29 16:12:17 +0200

    Add cluster

--- main.tf
+++ main.tf
@@ -3,3 +3,16 @@ resource "google_project" "viennacalling" {
   billing_account = "01B520-81D507-CD00E3"
   project_id      = "viennacalling"
 }
+
+resource "google_container_cluster" "viennacalling" {
+  name     = "viennacalling"
+  project  = google_project.viennacalling.name
+  location = "europe-west3"
+
+  enable_autopilot = true
+
+  network         = "default"
+  networking_mode = "VPC_NATIVE"
+
+  depends_on = [google_project_service.container]
+}
