cb545979b37ce12264b74b2b8820c019b28f33c5
Jan Jaworski <bleggbeard@protonmail.com>
2023-07-29 15:23:15 +0200

    Add provider and project

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "google_project" "viennacalling" {
+  name            = "viennacalling"
+  billing_account = "01B520-81D507-CD00E3"
+  project_id      = "viennacalling"
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "viennacalling"
+  region  = "europe-west3"
+  zone    = "europe-west3-c"
+}
